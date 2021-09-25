9623fef9ee651fd8ed11c88ea011cd175d82923d
Jeff DeWall <sr.jilarious@fastmail.net>
2021-09-25 16:15:29 -0600

    Added script to move origin repo over from gitlab to github

--- move_origin_repo.sh
+++ move_origin_repo.sh
@@ -25,26 +25,29 @@ for d in (ls -D1)
     #     git remote rename origin gitlab
     # end
 
-    set still_origin_url (git config remote.origin.url)
-    set github_origin_url (git config remote.origin.url | grep github.com)
-    if test "$still_origin_url" != ""
-        if test "$github_origin_url" = ""
-            echo "    ** An origin URL other than gitlab found ($still_origin_url), moving to backup"
-            git remote rename origin origin_old
-        else
-            echo "    - Found origin is already setup for Github"
-        end
-    end
+    # set still_origin_url (git config remote.origin.url)
+    # set github_origin_url (git config remote.origin.url | grep github.com)
+    # if test "$still_origin_url" != ""
+    #     if test "$github_origin_url" = ""
+    #         echo "    ** An origin URL other than gitlab found ($still_origin_url), moving to backup"
+    #         git remote rename origin origin_old
+    #     else
+    #         echo "    - Found origin is already setup for Github"
+    #     end
+    # end
 
-    set github_remote_url (git config remote.github.url)
-    if test "$github_remote_url" != ""
-        if test "$github_origin_url" = "" 
-            echo "    - Found github remote, duplicating to have origin "
-            git remote add origin $github_remote_url
-        end
-    else 
-        echo "    ** No Github remote found"
-    end 
+    # set github_remote_url (git config remote.github.url)
+    # if test "$github_remote_url" != ""
+    #     if test "$github_origin_url" = "" 
+    #         echo "    - Found github remote, duplicating to have origin "
+    #         git remote add origin $github_remote_url
+    #     end
+    # else 
+    #     echo "    ** No Github remote found"
+    # end 
+
+    set curr_branch (git branch --show-current)
+    git branch --set-upstream-to origin/$curr_branch
 
     cd $orig_dir
     echo "--------------"
