797a008a9e488d5d6d6c32c9dcffc74c74b82b94
Jeff DeWall <sr.jilarious@fastmail.net>
2023-05-13 09:04:22 -0600

    Commented out prev/next link in VimWiki, ctrl+[ looks to map to Esc, which caused annoying messages when pressing esc in normal mode without a link.

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -109,8 +109,8 @@ return {
       init = function () --replace 'config' with 'init'
         vim.g.vimwiki_list = {{path = '~/code/tech-notes/wiki', syntax = 'markdown', ext = '.md'}}
         vim.g.vimwiki_listsyms = " ◔◑◕✓"
-        vim.api.nvim_set_keymap('n', '<C-]>', '<Plug>VimwikiNextLink', {silent=true})
-        vim.api.nvim_set_keymap('n', '<C-[>', '<Plug>VimwikiPrevLink', {silent=true})
+        -- vim.api.nvim_set_keymap('n', '<C-]>', '<Plug>VimwikiNextLink', {silent=true})
+        -- vim.api.nvim_set_keymap('n', '<C-[>', '<Plug>VimwikiPrevLink', {silent=true})
         vim.api.nvim_set_keymap('n', '<S-Tab>', '<Plug>VimwikiDecreaseLvlWholeItem', {silent=true})
         vim.api.nvim_set_keymap('n', '<Tab>', '<Plug>VimwikiIncreaseLvlWholeItem', {silent=true})
       end,
