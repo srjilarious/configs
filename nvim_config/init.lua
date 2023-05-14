a6008f972eafe896dd72b1102edb597ab986759e
Jeff DeWall <sr.jilarious@fastmail.net>
2023-05-14 09:00:50 -0600

    Added in tabstop sizes, in C++ I was getting 2 spaces.

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -97,6 +97,10 @@ end
 vim.opt.list = true
 vim.opt.listchars:append({space = "·", lead = "·", tab = "» ", trail = "·"}) --, trail = '·', setbreak = "···", space = '·' }
 vim.opt.showbreak = "↪  "
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 
 return {
   plugins = {
