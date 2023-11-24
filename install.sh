a96b053556978e4fdead9d2a3873601d896e223f
Jeff DeWall <sr.jilarious@fastmail.net>
2023-11-24 12:22:00 -0700

    Added more git aliases, created better installation script.

--- install.sh
+++ install.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-SCRIPT=`realpath $0`
-SCRIPT_PATH=`dirname $SCRIPT`
+# Get the directory where the script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# We'll be adding our changes to the user's home directory.
-cd ~
+# Function to create symlink
+create_symlink() {
+    echo "Linking '$1' to '$2'..."
+    src="$script_dir/$1"
+    target=$2
 
-function create_config_link {
-  echo "Processing '~/$1'..."
-  if [ ! -f "~/$2" ]; then
-    echo "Creating link '~/$1' -> '$SCRIPT_PATH/$2'"
-    ln -s "$SCRIPT_PATH/$1" $2
-  else 
-    echo "Unable to link '$1' since it already exists."
-  fi
+    # Create target directory if it does not exist
+    mkdir -p "$(dirname "$target")"
+
+    # If the target file exists, backup it
+    if [ -f "$target" ]; then
+        if [ "$(readlink -f "$target")" != "$src" ]; then
+          echo "    - Backing up existing file to '${target}.bak'"
+          mv "$target" "${target}.bak"
+          ln -s "$src" "$target"
+        fi
+    else
+        # Create symlink if file does not exist.
+        echo "    - Creating link."
+        ln -s "$src" "$target"
+    fi
 }
 
-mkdir -p ~/.config
-create_config_link nvim ".config/nvim"
-create_config_link ".vim" ".vim" 
+# Create symlinks
+create_symlink gitconfig "$HOME/.config/git/config"
+create_symlink nvim_user.lua "$HOME/.config/nvim/lua/user/init.lua"
+# create_symlink tmux.conf "$HOME/.config/nvim/lua/user/init.lua"
 
-for i in dircolors my_bashrc gitconfig tmux.conf vimrc
-do
-  create_config_link "$i" ".$i" 
-done
+echo "Symlinks created successfully."
+
+# for i in dircolors my_bashrc gitconfig tmux.conf vimrc
+# do
+#   create_config_link "$i" ".$i" 
+# done
--- nvim_user.lua
+++ nvim_user.lua
@@ -176,12 +176,24 @@ return {
       keys = {"<leader>ww", "<leader>wt"},
       enabled = true,
       init = function () --replace 'config' with 'init'
-        vim.g.vimwiki_list = {{path = '~/code/tech-notes/wiki', syntax = 'markdown', ext = '.md'}}
+        vim.g.vimwiki_list = {
+          {
+            path = '~/code/tech-notes/wiki', 
+            syntax = 'markdown', 
+            ext = '.md'
+          },
+          { 
+            path = '~/vimwiki',
+            syntax = 'markdown',
+            ext = '.md',
+          }
+        }
         vim.g.vimwiki_listsyms = " ◔◑◕✓"
         -- vim.api.nvim_set_keymap('n', '<C-]>', '<Plug>VimwikiNextLink', {silent=true})
         -- vim.api.nvim_set_keymap('n', '<C-[>', '<Plug>VimwikiPrevLink', {silent=true})
         vim.api.nvim_set_keymap('n', '<S-Tab>', '<Plug>VimwikiDecreaseLvlWholeItem', {silent=true})
         vim.api.nvim_set_keymap('n', '<Tab>', '<Plug>VimwikiIncreaseLvlWholeItem', {silent=true})
+        vim.api.nvim_set_keymap('n', '<Leader>w2', ':VimwikiDiaryIndex 2<CR>:VimwikiMakeDiaryNote<CR>', { noremap = true, silent = true })
       end,
     },
     {
